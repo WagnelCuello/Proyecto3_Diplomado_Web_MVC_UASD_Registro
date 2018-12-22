37fa4fce256730e00ca7a0ae529bf3682cbb34dd
wagnel <wagnelcuello@gmail.com>
2018-12-22 17:44:16 -0400

    ultima subida

--- Proyecto3_Diplomado_Web_MVC_UASD_Registro/Controllers/HomeController.cs
+++ Proyecto3_Diplomado_Web_MVC_UASD_Registro/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto3_Diplomado_Web_MVC_UASD_Registro.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,26 @@ namespace Proyecto3_Diplomado_Web_MVC_UASD_Registro.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var coches = new List<Coche>()
+            {
+                new Coche(){ Tipo = "Jeep", Marca = "BMW", Modelo = "X6", Color = "Azul" },
+                new Coche(){ Tipo = "Auto", Marca = "Mercedes", Modelo = "A200", Color = "Blanco" },
+                new Coche(){ Tipo = "Jeep", Marca = "Porsche", Modelo = "Cayanne", Color = "Negro" },
+                new Coche(){ Tipo = "Auto", Marca = "Ford", Modelo = "Mustang", Color = "Rojo" }
+            };
+            return View(coches);
+        }
+
+        public ActionResult Personas()
+        {
+            var personas = new List<Persona>()
+            {
+                new Persona(){ Nombre = "Juan", Apellido = "Santos", Direccion = "por ahi", Telefono = "809-522-6336" },
+                new Persona(){ Nombre = "Pedro", Apellido = "Martinez", Direccion = "lejisimo", Telefono = "809-522-6336" },
+                new Persona(){ Nombre = "Mario", Apellido = "Espinosa", Direccion = "bastante lejos", Telefono = "809-522-6336" },
+                new Persona(){ Nombre = "Veneno", Apellido = "Perez", Direccion = "en pekin", Telefono = "809-522-6336" }
+            };
+            return View(personas);
         }
     }
 }
---
+++ Proyecto3_Diplomado_Web_MVC_UASD_Registro/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto3_Diplomado_Web_MVC_UASD_Registro.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
