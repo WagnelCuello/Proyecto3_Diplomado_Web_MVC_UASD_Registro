71afebf95fb329268af91d524d75ef3cec73b7fd
wagnel <wagnelcuello@gmail.com>
2018-12-22 17:24:29 -0400

    subida inicial

---
+++ Proyecto3_Diplomado_Web_MVC_UASD_Registro/Models/Coche.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto3_Diplomado_Web_MVC_UASD_Registro.Models
+{
+    public class Coche
+    {
+        public string Tipo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+    }
+}
